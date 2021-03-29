9e4d5461b1d67f1158055769d53f6b9c367affda
Andres Gutierrez <andresgutierrez535@gmail.com>
2021-03-28 23:27:54 -0400

    test deploy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,89 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <main>
-	<h1>Hello world!</h1>
+    <h1>Block Parties</h1>
 
-	<Counter />
+    <div>
+        <h3>A <em>Revolution</em> in Investing</h3>
+        <p>Leveraging the blockchain to make investing as <em>safe</em>, <em>easy</em>, and <em>social</em> as ever.</p>
+    </div>
 
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+    <div class="email-form">
+        <h5>Reserve your seat in the limited access beta</h5>
+        <div>
+            <input type="email" />
+            <button>Submit</button>
+        </div>
+        <p>We won't use your email for anything other than letting you know when the beta opens.</p>
+    </div>
 </main>
 
 <style lang="scss">
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
+    // :root {
+    //     font-family: "Open Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell,
+    //         "Helvetica Neue", sans-serif;
+    // }
 
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
+    h1,
+    h3,
+    h5,
+    p {
+        color: white;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
+    :global(body) {
+        margin: 0;
+        height: 100vh;
+        background: linear-gradient(235.71deg, #400a84 7.8%, #1b0a30 92.44%);
+    }
 
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        margin: auto;
+    }
 
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
+    .email-form {
+        width: 459px;
+        height: 171px;
+        margin: auto;
 
-		p {
-			max-width: none;
-		}
-	}
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 24px;
+
+        background: linear-gradient(0deg, #040a11, #040a11), #0352af;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 8px;
+
+        h5 {
+            margin: 0;
+        }
+
+        input {
+            height: 48px;
+
+            border: none;
+            background: rgba(142, 145, 148, 0.1);
+            border-radius: 4px;
+            color: white;
+        }
+
+        button {
+            height: 48px;
+            border: none;
+            background: #400a84;
+            border-radius: 4px;
+
+            padding: 8px;
+            color: white;
+        }
+
+        p {
+            font-size: 12px;
+            font-weight: 300;
+        }
+    }
 </style>
